a490179803b93b76f0f4dca1de6c6164b7239aa2
Tarel-Github <sms_stmax@naver.com>
2023-03-03 17:24:20 +0900

    3월 3일 금요일

---
+++ Day04/db20_outer_join_query.sql
@@ -0,0 +1,49 @@
+-- 쇼핑몰에 가입하고 물건을 한 번도 구입하지 않은 회원까지 모두 출력
+SELECT u.*
+	 , b.userID
+	 , b.prodName
+     , b.groupName
+     , b.price
+     , b.amount
+  FROM usertbl AS u
+  LEFT OUTER JOIN buytbl AS b
+    ON u.userID = b.userID
+ WHERE b.userID IS NULL;
+ 
+-- 학생중에 동아리에 가입하지 않은 학생
+SELECT s.stdName, s.addr
+     , j.num, j.clubname
+  FROM stdtbl AS s
+  LEFT OUTER JOIN stdclubtbl AS j
+    ON s.stdName = j.stdName
+ INNER JOIN clubtbl AS c
+    ON c.clubName = j.clubName
+UNION -- 집합(위 아래 두 쿼리의 결과를 하나로 합쳐주는 기능)
+--  
+ SELECT s.stdName, s.addr
+     , j.num, c.clubname, c.roomNo
+  FROM stdtbl AS s
+ RIGHT OUTER JOIN stdclubtbl AS j
+    ON s.stdName = j.stdName
+ INNER JOIN clubtbl AS c
+    ON c.clubName = j.clubName;   
+    
+-- inner join의 약식표현(권하지 않음)
+SELECT u.*
+	 , b.userID
+	 , b.prodName
+     , b.groupName
+     , b.price
+     , b.amount
+  FROM usertbl AS u, buytbl AS b
+ WHERE u.userID = b.userID;
+    
+-- IN 조건이 만족하는 것
+-- NOT IN 조건 만족하지 않는 것
+SELECT name
+     , height
+  FROM usertbl
+ WHERE height NOT IN (SELECT height
+						FROM usertbl
+					   WHERE name LIKE '김경호%')
+                        
